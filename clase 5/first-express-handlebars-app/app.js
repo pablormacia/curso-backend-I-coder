1074aaee3834ccc547142d0294384e45f5a6afa0
pablormacia <pr.macia@gmail.com>
2026-06-07 18:46:46 -0300

    Clase 5 - Config inicial express handlebars

---
+++ clase 5/first-express-handlebars-app/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const exphbs  = require('express-handlebars');
+const mayusculas = require('./helpers/mayusculas');
+
+const app = express();
+
+// Configurar Handlebars como motor de vistas
+app.engine('handlebars', exphbs.engine({
+  defaultLayout: 'main',
+  helpers: {
+    // Aquí se registran helpers personalizados
+      /* mayusculas: (texto) => {
+        return texto.toUpperCase();
+      } */
+      mayusculas
+
+  }
+}));
+
+
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+// Ruta principal
+app.get('/', (req, res) => {
+  res.render('home', {
+    nombre: 'Pablo'
+  });
+});
+
+
+app.listen(3000, () => {
+  console.log('Servidor escuchando en puerto 3000');
+});
---
+++ clase 5/first-express-handlebars-app/helpers/mayusculas.js
@@ -0,0 +1,5 @@
+function mayusculas(texto) {
+  return texto.toUpperCase();
+}
+
+module.exports = mayusculas;
